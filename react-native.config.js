b12e3b7bc4eea68f421cb8997a011981fdf8c79c
MRRaihan <raihantfp@gmail.com>
2022-09-08 16:56:57 +0600

    react-native-vector-icons successfully added

--- react-native.config.js
+++ react-native.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    dependencies: {
-      'react-native-vector-icons': {
-        platforms: {
-          ios: null,
-        },
-      },
+    project: {
+      ios: {},
+      android: {},
     },
+    assets: ['./assets/Fonts/'],
   };
+
+  
